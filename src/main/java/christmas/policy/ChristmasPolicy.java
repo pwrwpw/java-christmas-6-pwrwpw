2870fa86957f1ac2c1e5522afef189ee9bc14d07
pwrwpw <dlgustjr9912@gmail.com>
2023-11-11 04:18:19 +0900

    feat: 날짜 최소 최대 정책 추가

--- src/main/java/christmas/policy/ChristmasPolicy.java
+++ src/main/java/christmas/policy/ChristmasPolicy.java
@@ -2,6 +2,8 @@ package christmas.policy;
 
 public class ChristmasPolicy {
     public static final int DECEMBER = 12;
+    public static final int DEC_MINIMUM_DAY = 1;
+    public static final int DEC_MAXIMUM_DAY = 31;
     public static final int MINIMUM_ORDER_AMOUNT = 10_000;
     public static final int MAX_MENU_ITEMS = 20;
 }
