f4d3563514af0475526d85249e48535d62cf5e81
pwrwpw <dlgustjr9912@gmail.com>
2023-11-11 04:38:37 +0900

    refactor : date -> day 리팩토링

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,7 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
 
-    public String getExpectedVisitDate() {
+    public String getExpectedVisitDay() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         return readLine();
     }
