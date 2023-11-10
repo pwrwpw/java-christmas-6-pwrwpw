69f35c2fe61efcc5d95c1be4c405981ce068cc5b
pwrwpw <dlgustjr9912@gmail.com>
2023-11-11 02:53:16 +0900

    feat: 정책과 출력메시지를 통해 사용자에게 출력 및 입력을 받는 클래스 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,62 @@
+package christmas.view;
+
+import christmas.policy.ChristmasPolicy;
+
+public class OutputView {
+
+    public void printWelcomeMessage() {
+        OutputViewMessages.WELCOME.print(ChristmasPolicy.DECEMBER);
+    }
+
+    public void printEventPreviewMessage(int userInputDate) {
+        OutputViewMessages.EVENT_PREVIEW.print(ChristmasPolicy.DECEMBER, userInputDate);
+    }
+
+    public void printEventGuidelines() {
+        OutputViewMessages.EVENT_GUIDELINES.print();
+    }
+
+    public void printMinimumOrderAmountNotice() {
+        OutputViewMessages.MINIMUM_ORDER_AMOUNT.print(ChristmasPolicy.MINIMUM_ORDER_AMOUNT);
+    }
+
+    public void printBeverageOrderRestrictionNotice() {
+        OutputViewMessages.BEVERAGE_ORDER_RESTRICTION.print();
+    }
+
+    public void printMaxMenuItemsNotice() {
+        OutputViewMessages.MAX_MENU_ITEMS.print(ChristmasPolicy.MAX_MENU_ITEMS);
+    }
+
+    public void printOrderMenuMessage() {
+        OutputViewMessages.ORDER_MENU.print();
+    }
+
+    public void printTotalPriceMessage() {
+        OutputViewMessages.TOTAL_PRICE.print();
+    }
+
+    public void printPresentMenuMessage() {
+        OutputViewMessages.PRESENT_MENU.print();
+    }
+
+    public void printBenefitMessage() {
+        OutputViewMessages.BENEFIT_DETAILS.print();
+    }
+
+    public void printTotalBenefitPriceMessage() {
+        OutputViewMessages.TOTAL_BENEFIT_AMOUNT.print();
+    }
+
+    public void printExpectedPaymentPriceMessage() {
+        OutputViewMessages.EXPECTED_PAYMENT.print();
+    }
+
+    public void printEventBadgeMessage() {
+        OutputViewMessages.EVENT_BADGE.print(ChristmasPolicy.DECEMBER);
+    }
+
+    public void printNoBenefitMessage() {
+        OutputViewMessages.NO_BENEFIT.print();
+    }
+}
