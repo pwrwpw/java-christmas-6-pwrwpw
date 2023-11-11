7f4dab1e9ecfb9c1c95a1a9b3d5dd45add34c580
pwrwpw <dlgustjr9912@gmail.com>
2023-11-11 14:04:25 +0900

    feat: 사용자가 입력한 메뉴를 parse하는 메서드 추가

--- src/main/java/christmas/utils/Parser.java
+++ src/main/java/christmas/utils/Parser.java
@@ -1,8 +1,44 @@
 package christmas.utils;
 
+import christmas.domain.MenuItems;
+import christmas.domain.SelectMenu;
+import christmas.exception.domain.visitdate.InvalidOrderException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Parser {
 
     public static int parseInteger(String input) {
         return Integer.parseInt(input);
     }
+
+    public static List<String> splitMenuItems(String input) {
+        StringValidator.isBlankOrEmpty(input);
+        String[] parsedItems = input.split(",");
+
+        return Arrays.asList(parsedItems);
+    }
+
+    public static MenuItems parseMenuItems(List<String> splitMenuItems) {
+        List<SelectMenu> parsedItems = new ArrayList<>();
+
+        for (String selectMenu : splitMenuItems) {
+            String[] selectMenuInfo = selectMenu.split("-");
+            if (selectMenuInfo.length != 2) {
+                throw new InvalidOrderException();
+            }
+            String name = selectMenuInfo[0].trim();
+            String d = selectMenuInfo[1].trim();
+            if(FormatValidator.isNumber(d)) {
+                throw new InvalidOrderException();
+            }
+            int quantity = Integer.parseInt(d);
+            parsedItems.add(new SelectMenu(name, quantity));
+
+        }
+
+        return new MenuItems(parsedItems);
+    }
+
 }
