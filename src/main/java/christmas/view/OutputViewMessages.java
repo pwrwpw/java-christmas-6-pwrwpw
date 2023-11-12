99b237d22a7fd0ef53b4d69d423407b5dd9c2a5c
pwrwpw <dlgustjr9912@gmail.com>
2023-11-13 06:54:07 +0900

    feat: 개행 추가시킴으로 출력을 요구사항과 맞춤

--- src/main/java/christmas/view/OutputViewMessages.java
+++ src/main/java/christmas/view/OutputViewMessages.java
@@ -8,15 +8,15 @@ public enum OutputViewMessages {
     BEVERAGE_ORDER_RESTRICTION("음료만 주문 시, 주문할 수 없습니다."),
     MAX_MENU_ITEMS("메뉴는 한 번에 최대 %d개까지만 주문할 수 있습니다."),
     MAX_MENU_ITEMS_EX("(e.g. 시저샐러드-1, 티본스테이크-1, 크리스마스파스타-1, 제로콜라-3, 아이스크림-1의 총개수는 7개)"),
-    ORDER_MENU("<주문 메뉴>"),
-    TOTAL_PRICE("<할인 전 총주문 금액>"),
+    ORDER_MENU("\n<주문 메뉴>"),
+    TOTAL_PRICE("\n<할인 전 총주문 금액>"),
     TOTAL_PRICE_OUTPUT("%d원"),
     TOTAL_BENEFIT_OUTPUT("-%d원"),
-    PRESENT_MENU("<증정 메뉴>"),
-    BENEFIT_DETAILS("<혜택 내역>"),
-    TOTAL_BENEFIT_AMOUNT("<총혜택 금액>"),
-    EXPECTED_PAYMENT("<할인 후 예상 결제 금액>"),
-    EVENT_BADGE("<%d월 이벤트 배지>"),
+    PRESENT_MENU("\n<증정 메뉴>"),
+    BENEFIT_DETAILS("\n<혜택 내역>"),
+    TOTAL_BENEFIT_AMOUNT("\n<총혜택 금액>"),
+    EXPECTED_PAYMENT("\n<할인 후 예상 결제 금액>"),
+    EVENT_BADGE("\n<%d월 이벤트 배지>"),
     NO_BENEFIT("없음");
 
     private final String text;
