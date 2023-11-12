fc534dd596634fc36b2f78a108848382d79b82b7
pwrwpw <dlgustjr9912@gmail.com>
2023-11-13 05:37:23 +0900

    feat: 의존성 주입하도록 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,13 +1,13 @@
 package christmas;
 
 import christmas.controller.ChristmasController;
-import christmas.service.OrderService;
+import christmas.service.ChristmasDiscountService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        ChristmasController christmasController = new ChristmasController(new OutputView(), new InputView());
+        ChristmasController christmasController = new ChristmasController(new OutputView(), new InputView(), new ChristmasDiscountService());
         christmasController.run();
     }
 }
