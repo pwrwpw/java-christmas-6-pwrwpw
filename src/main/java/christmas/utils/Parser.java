2a13dcae2439a0dadc8aa78747e85dce931aec48
pwrwpw <dlgustjr9912@gmail.com>
2023-11-14 05:55:09 +0900

    refactor: Parser는 Parsing만 하도록 수정

--- src/main/java/christmas/utils/Parser.java
+++ src/main/java/christmas/utils/Parser.java
@@ -10,33 +10,14 @@ import java.util.List;
 public class Parser {
 
     public static int parseInteger(String input) {
+        if(FormatValidator.isNumber(input)) {
+            throw new InvalidOrderException();
+        }
         return Integer.parseInt(input);
     }
 
     public static List<String> splitMenuItems(String input) {
         StringValidator.isBlankOrEmpty(input);
-        String[] parsedItems = input.split(",");
-
-        return Arrays.asList(parsedItems);
+        return Arrays.asList(input.split(","));
     }
-
-    public static SelectMenus parseMenuItems(List<String> splitMenuItems) {
-        List<SelectMenu> parsedItems = new ArrayList<>();
-
-        for (String selectMenu : splitMenuItems) {
-            String[] selectMenuInfo = selectMenu.split("-");
-            if (selectMenuInfo.length != 2) {
-                throw new InvalidOrderException();
-            }
-            String name = selectMenuInfo[0].trim();
-            String d = selectMenuInfo[1].trim();
-            if (FormatValidator.isNumber(d)) {
-                throw new InvalidOrderException();
-            }
-            int quantity = Integer.parseInt(d);
-            parsedItems.add(new SelectMenu(name, quantity));
-        }
-        return new SelectMenus(parsedItems);
-    }
-
 }
