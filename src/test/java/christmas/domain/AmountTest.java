117276a9e8a8fe4333141ee5c60c3e43a4a3ee89
pwrwpw <dlgustjr9912@gmail.com>
2023-11-13 14:55:51 +0900

    refactor: import시 * 삭제

--- src/test/java/christmas/domain/AmountTest.java
+++ src/test/java/christmas/domain/AmountTest.java
@@ -1,6 +1,9 @@
 package christmas.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
