7cad7561ec740e59a3cd14a41a8fd97816e22f96
pwrwpw <dlgustjr9912@gmail.com>
2023-11-14 05:55:38 +0900

    refactor: 생성을 SelectMenus에서 함으로써 책임을 SelectMenus에서 가짐

--- src/main/java/christmas/domain/SelectMenus.java
+++ src/main/java/christmas/domain/SelectMenus.java
@@ -1,18 +1,47 @@
 package christmas.domain;
 
 import christmas.exception.domain.visitdate.InvalidOrderException;
-import java.util.HashSet;
+import christmas.utils.Parser;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
-public record SelectMenus(List<SelectMenu> items) {
-    public SelectMenus {
+public class SelectMenus {
+    private List<SelectMenu> items;
+
+    public SelectMenus(List<SelectMenu> items) {
         validateDuplicate(items);
+        this.items = items;
+    }
+
+    public List<SelectMenu> items() {
+        return items;
+    }
+    public static SelectMenus from(List<String> menuItemsStrings) {
+        List<SelectMenu> parsedItems = new ArrayList<>();
+        for (String itemString : menuItemsStrings) {
+            parsedItems.add(parseMenuItem(itemString));
+        }
+        return new SelectMenus(parsedItems);
+    }
+
+    private static SelectMenu parseMenuItem(String itemString) {
+        String[] parts = itemString.split("-");
+        if (parts.length != 2) {
+            throw new InvalidOrderException();
+        }
+
+        String name = parts[0].trim();
+        int quantity = Parser.parseInteger(parts[1].trim());
+        return new SelectMenu(name, quantity);
     }
 
     private void validateDuplicate(List<SelectMenu> items) {
-        Set<SelectMenu> uniqueItems = new HashSet<>(items);
-        if (uniqueItems.size() != items.size()) {
+        boolean hasDuplicate = items.stream()
+                .map(SelectMenu::menuName)
+                .collect(Collectors.toSet())
+                .size() != items.size();
+        if (hasDuplicate) {
             throw new InvalidOrderException();
         }
     }
