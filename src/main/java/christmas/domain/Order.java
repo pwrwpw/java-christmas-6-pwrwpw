304457796d1ea7e93dbceec68a80b359f6763ccd
pwrwpw <dlgustjr9912@gmail.com>
2023-11-14 04:45:51 +0900

    feat: 빈리스트, 음수 주문이 들어올경우 예외처리 해줌으로써 한번더 걸러줌

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import christmas.exception.domain.visitdate.InvalidOrderException;
+import christmas.exception.domain.visitdate.InvalidPriceException;
 import christmas.policy.ChristmasPolicy;
 
 public class Order {
@@ -9,6 +11,7 @@ public class Order {
     private final Amount totalAmount;
 
     public Order(VisitDate visitDate, SelectMenus selectMenus, Amount totalAmount) {
+        validateOrder(visitDate, selectMenus, totalAmount);
         this.visitDate = visitDate;
         this.selectMenus = selectMenus;
         this.totalAmount = totalAmount;
@@ -17,9 +20,11 @@ public class Order {
     public boolean isEligibleForPresent() {
         return getTotalAmount() > ChristmasPolicy.PRESENT_THRESHOLD_AMOUNT;
     }
+
     public int calculateFinalPayment(int discountAmount) {
         return getTotalAmount() - discountAmount;
     }
+
     public VisitDate getVisitDate() {
         return visitDate;
     }
@@ -34,5 +39,22 @@ public class Order {
 
     public int getVisitDay() {
         return visitDate.getDay();
+    }
+
+    private void validateTotalAmount(Amount totalAmount) {
+        if (totalAmount.value() < 0) {
+            throw new InvalidPriceException();
+        }
+    }
+
+    private void validateSelectMenus(SelectMenus selectMenus) {
+        if (selectMenus.items().isEmpty()) {
+            throw new InvalidOrderException();
+        }
+    }
+
+    private void validateOrder(VisitDate visitDate, SelectMenus selectMenus, Amount totalAmount) {
+        validateTotalAmount(totalAmount);
+        validateSelectMenus(selectMenus);
     }
 }
