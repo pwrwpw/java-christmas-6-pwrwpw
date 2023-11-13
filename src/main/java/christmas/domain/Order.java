2b2cdda9422007a4a4ed9348bbc64db3b72e86db
pwrwpw <dlgustjr9912@gmail.com>
2023-11-13 10:48:25 +0900

    refactor: 바깥에서 getter 사용보단 내부에서 처리후 반환하도록 수정

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,17 +1,25 @@
 package christmas.domain;
 
+import christmas.policy.ChristmasPolicy;
+
 public class Order {
 
     private final VisitDate visitDate;
-    private final SelectMenu selectMenu;
+    private final SelectMenus selectMenus;
     private final Amount totalAmount;
 
-    public Order(VisitDate visitDate, SelectMenu selectMenu, Amount totalAmount) {
+    public Order(VisitDate visitDate, SelectMenus selectMenus, Amount totalAmount) {
         this.visitDate = visitDate;
-        this.selectMenu = selectMenu;
+        this.selectMenus = selectMenus;
         this.totalAmount = totalAmount;
     }
 
+    public boolean isEligibleForPresent() {
+        return getTotalAmount() > ChristmasPolicy.PRESENT_THRESHOLD_AMOUNT;
+    }
+    public int calculateFinalPayment(int discountAmount) {
+        return getTotalAmount() - discountAmount;
+    }
     public int getVisitDay() {
         return visitDate.getDay();
     }
