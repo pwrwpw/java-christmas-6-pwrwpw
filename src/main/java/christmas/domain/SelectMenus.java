d1079d29f11a480e0e1d8099526670c474e57fb0
pwrwpw <dlgustjr9912@gmail.com>
2023-11-13 10:48:32 +0900

    refactor: 의미없는 기능 삭제

--- src/main/java/christmas/domain/SelectMenus.java
+++ src/main/java/christmas/domain/SelectMenus.java
@@ -1,7 +1,6 @@
 package christmas.domain;
 
 import christmas.exception.domain.visitdate.InvalidOrderException;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,11 +8,6 @@ import java.util.Set;
 public record SelectMenus(List<SelectMenu> items) {
     public SelectMenus {
         validateDuplicate(items);
-    }
-
-    @Override
-    public List<SelectMenu> items() {
-        return Collections.unmodifiableList(items);
     }
 
     private void validateDuplicate(List<SelectMenu> items) {
