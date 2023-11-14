4dcb10d3762ffa3447406ebf203a693a5ee1bb98
pwrwpw <dlgustjr9912@gmail.com>
2023-11-14 20:13:23 +0900

    refactor: package명 수정

--- src/test/java/christmas/policy/MenuPolicyTest.java
+++ src/test/java/christmas/policy/MenuPolicyTest.java
@@ -1,6 +1,5 @@
-package christmas;
+package christmas.policy;
 
-import christmas.policy.MenuPolicy;
 import christmas.exception.domain.visitdate.InvalidOrderException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
