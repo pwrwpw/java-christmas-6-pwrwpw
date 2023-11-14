653724c32ed26ae78da49a0d5f51da656a789607
pwrwpw <dlgustjr9912@gmail.com>
2023-11-15 06:53:41 +0900

    refactor: 컨트롤러 생성되는 구조 변경

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,15 +1,25 @@
 package christmas;
 
 import christmas.controller.ChristmasController;
+import christmas.domain.Order;
 import christmas.service.ChristmasDiscountService;
 import christmas.service.ChristmasService;
 import christmas.service.ChristmasOrderService;
+import christmas.service.DiscountService;
+import christmas.service.OrderService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        ChristmasController christmasController = new ChristmasController(new OutputView(), new InputView(), new ChristmasService(new ChristmasDiscountService(), new ChristmasOrderService()));
+        OrderService orderService = new ChristmasOrderService();
+        DiscountService discountService = new ChristmasDiscountService();
+        ChristmasService christmasService = new ChristmasService(discountService, orderService);
+
+        ChristmasController christmasController = new ChristmasController(
+                new OutputView(),
+                new InputView(),
+                christmasService);
         christmasController.run();
     }
 }
