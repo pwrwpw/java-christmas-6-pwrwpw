a4132ba04b281bbf5f22965f5f8a987fee196a7a
pwrwpw <dlgustjr9912@gmail.com>
2023-11-15 01:28:33 +0900

    refactor: 주의사항 출력시 강조 표시 추가

--- src/main/java/christmas/view/OutputViewMessages.java
+++ src/main/java/christmas/view/OutputViewMessages.java
@@ -3,7 +3,7 @@ package christmas.view;
 public enum OutputViewMessages {
     WELCOME("안녕하세요! 우테코 식당 %d월 이벤트 플래너 입니다"),
     EVENT_PREVIEW("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
-    EVENT_GUIDELINES("주문시 이벤트 주의사항"),
+    EVENT_GUIDELINES("<주문시 이벤트 주의사항>"),
     MINIMUM_ORDER_AMOUNT("총주문 금액 %,d원 이상부터 이벤트가 적용됩니다."),
     BEVERAGE_ORDER_RESTRICTION("음료만 주문 시, 주문할 수 없습니다."),
     MAX_MENU_ITEMS("메뉴는 한 번에 최대 %d개까지만 주문할 수 있습니다."),
